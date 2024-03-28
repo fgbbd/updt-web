38173509cc655d0ea65dadf347999f98b74ed545
Álvaro <alvarobotdc@gmail.com>
2024-03-28 23:47:33 +0100

    a

---
+++ js/index.js
@@ -0,0 +1,4 @@
+function changeButton() {
+    document.getElementById("myButton").innerHTML = "Te has unido";
+    document.getElementById("myButton").setAttribute("href", "htttps://google.com");
+}
