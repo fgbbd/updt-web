bb64d4244256175a0fe8be98056afe26c7cc6c6d
Álvaro <alvarobotdc@gmail.com>
2024-03-29 23:30:03 +0100

    js index fixed

--- js/index.js
+++ js/index.js
@@ -4,17 +4,17 @@ function changeButton() {
 }
 
 function changeButton2() {
-    window.open("index.html","_self")
+    window.open("https://updt1.netlify.app","_self")
 }
 
 function changeButton3() {
-    window.open("ayudante-updt/info.html","_self")
+    window.open("https://updt1.netlify.app/ayudante-updt/info","_self")
 }
 
 function changeButton4() {
-    window.open("postulacion.html","_self")
+    window.open("https://updt1.netlify.app/postulacion","_self")
 }
 
 function changeButton5() {
-    window.open("contacto.html","_self")
+    window.open("https://updt1.netlify.app/contacto","_self")
 }
