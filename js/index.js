b23b32cecf3601470262b005585504e133f37ec7
Álvaro <alvarobotdc@gmail.com>
2024-03-29 13:01:25 +0100

    Barra de navegacion completada V1

--- js/index.js
+++ js/index.js
@@ -2,3 +2,19 @@ function changeButton() {
     document.getElementById("myButton").innerHTML = "Joined";
     window.open("https://discord.gg/xXxekfxCQ2","_blank")
 }
+
+function changeButton2() {
+    window.open("index.html","_self")
+}
+
+function changeButton3() {
+    window.open("ayudante-updt.html","_self")
+}
+
+function changeButton4() {
+    window.open("postulacion.html","_self")
+}
+
+function changeButton5() {
+    window.open("contacto.html","_self")
+}
